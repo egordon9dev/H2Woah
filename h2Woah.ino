86371177bc2433ff24f72b4d80834bb1557ad148
egordon9dev <egordon9dev@gmail.com>
2018-03-18 01:45:48 -0500

    ..

---
+++ h2Woah.ino
@@ -0,0 +1,12 @@
+int valve = 2, light = 3;
+void setup() {
+  Serial.begin(9600);
+  pinMode(valve, OUTPUT);
+  digitalWrite(valve, 0);
+  pinMODE(light, OUTPUT);
+  digitalWrite(light, 0);
+}
+
+void loop() {
+  
+}
